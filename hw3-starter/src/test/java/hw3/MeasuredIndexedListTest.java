44ac377aedba2a9f6d76d8f7dc7b3d9b2d6ec599
Alex-Shane <ashane1230@gmail.com>
2023-02-26 22:02:23 -0500

    added tons of unit tests

--- hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
+++ hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
@@ -1,11 +1,13 @@
 package hw3;
 
+import exceptions.IndexException;
 import hw3.list.MeasuredIndexedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MeasuredIndexedListTest {
 
@@ -31,5 +33,139 @@ public class MeasuredIndexedListTest {
     assertEquals(0, measuredIndexedList.mutations());
   }
 
-  // TODO Add more tests
+  @Test
+  @DisplayName("MeasuredIndexedList handles single access properly")
+  void testSingleAccessInList() {
+    measuredIndexedList.get(3);
+    assertEquals(1, measuredIndexedList.accesses());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList handles multiple accesses properly")
+  void testMultipleAccessesInList() {
+    measuredIndexedList.get(4);
+    measuredIndexedList.get(5);
+    measuredIndexedList.get(2);
+    assertEquals(3, measuredIndexedList.accesses());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList count() works properly for one non null value")
+  void testCountWithSingleNonNullValue() {
+    measuredIndexedList.put(1, 4);
+    int count = measuredIndexedList.count(4);
+    assertEquals(1, count);
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList count() works properly for multiple occurrences of a non null value")
+  void testCountWithMultipleOccurrencesOfNonNullValue() {
+    measuredIndexedList.put(1, 4);
+    measuredIndexedList.put(8, 4);
+    int count = measuredIndexedList.count(4);
+    assertEquals(2, count);
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList count() works properly for one null value")
+  void testCountWithOneNullValue() {
+    measuredIndexedList.put(1,null);
+    int count = measuredIndexedList.count(null);
+    assertEquals(1, count);
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList count() works properly for multiple null values")
+  void testCountWithMultipleNullValues() {
+    measuredIndexedList.put(1,null);
+    measuredIndexedList.put(4, null);
+    int count = measuredIndexedList.count(null);
+    assertEquals(2, count);
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList handles accesses properly with count method")
+  void testAccessesWithCountMethodInList() {
+    measuredIndexedList.put(1, 4);
+    int count = measuredIndexedList.count(4); // count should call get() 15 times
+    assertEquals(15, measuredIndexedList.accesses());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList doesn't increase accesses for get() with negative index")
+  void testAccessesAfterGetWithNegativeIndex() {
+    try {
+      measuredIndexedList.get(-1);
+      fail("get() didn't catch invalid index");
+    } catch (IndexException ex) {
+      assertEquals(0, measuredIndexedList.accesses());
+    }
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList doesn't increase accesses for get() with index >= length")
+  void testAccessesAfterGetWithTooLargeIndex() {
+    try {
+      measuredIndexedList.get(15);
+      fail("get() didn't catch invalid index");
+    } catch (IndexException ex) {
+      assertEquals(0, measuredIndexedList.accesses());
+    }
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList doesn't increase mutations for put() with index >= length")
+  void testMutationsAfterPutWithTooLargeIndex() {
+    try {
+      measuredIndexedList.put(15, 6);
+      fail("put() didn't catch invalid index");
+    } catch (IndexException ex) {
+      assertEquals(0, measuredIndexedList.mutations());
+    }
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList doesn't increase mutations for put() with negative index")
+  void testMutationsAfterPutWithNegativeIndex() {
+    try {
+      measuredIndexedList.put(-1, 6);
+      fail("put() didn't catch invalid index");
+    } catch (IndexException ex) {
+      assertEquals(0, measuredIndexedList.mutations());
+    }
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList handles single mutation properly")
+  void testMutationsAfterSinglePutCall() {
+    measuredIndexedList.put(4, 39);
+    assertEquals(1, measuredIndexedList.mutations());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList handles multiple mutations properly")
+  void testMutationsAfterMultiplePutCalls() {
+    measuredIndexedList.put(5, 38);
+    measuredIndexedList.put (8,27);
+    assertEquals(2, measuredIndexedList.mutations());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList properly resets accesses")
+  void testResetWithAccesses() {
+    measuredIndexedList.get(4);
+    measuredIndexedList.reset();
+    assertEquals(0, measuredIndexedList.accesses());
+  }
+
+  @Test
+  @DisplayName("MeasuredIndexedList properly resets mutations")
+  void testResetWithMutations() {
+    measuredIndexedList.put(8,47);
+    measuredIndexedList.reset();
+    assertEquals(0, measuredIndexedList.mutations());
+  }
+
+
+
 }
