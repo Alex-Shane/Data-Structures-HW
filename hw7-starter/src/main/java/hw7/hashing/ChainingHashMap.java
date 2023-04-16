f6b91087caad66b5bbb7759568b20b46512f81cb
Alex-Shane <ashane1230@gmail.com>
2023-04-15 20:23:10 -0400

    implemented basic features for chaining hash map

--- hw7-starter/src/main/java/hw7/hashing/ChainingHashMap.java
+++ hw7-starter/src/main/java/hw7/hashing/ChainingHashMap.java
@@ -2,8 +2,11 @@ package hw7.hashing;
 
 import hw7.Map;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class ChainingHashMap<K, V> implements Map<K, V> {
+  private LinkedList<Pair<K,V>> [] map;
+  private int size;
   @Override
   public void insert(K k, V v) throws IllegalArgumentException {
     // TODO Implement Me!
@@ -17,30 +20,74 @@ public class ChainingHashMap<K, V> implements Map<K, V> {
 
   @Override
   public void put(K k, V v) throws IllegalArgumentException {
-    // TODO Implement Me!
+    if (k == null || !has(k)) {
+      throw new IllegalArgumentException();
+    }
+    int index = getIndex(k);
+    LinkedList<Pair<K, V>> chain = map[index];
+    Pair<K, V> targetPair = findPairInChain(k, chain);
+    int indexInChain = chain.indexOf(targetPair);
+    chain.set(indexInChain, new Pair<>(k,v));
   }
 
   @Override
   public V get(K k) throws IllegalArgumentException {
-    // TODO Implement Me!
+    if (k == null || !has(k)) {
+      throw new IllegalArgumentException();
+    }
+    int index = getIndex(k);
+    LinkedList<Pair<K, V>> chain = map[index];
+    Pair<K, V> targetPair = findPairInChain(k, chain);
+    if (targetPair != null) {
+      return targetPair.getValue();
+    }
     return null;
   }
 
   @Override
   public boolean has(K k) {
-    // TODO Implement Me!
-    return false;
+    if (k == null) {
+      return false;
+    }
+    int index = getIndex(k);
+    return map[index].size() != 0;
+  }
+
+  private Pair<K, V> findPairInChain(K k, LinkedList<Pair<K, V>> chain) {
+    for (Pair<K, V> pair : chain) {
+      if (pair.getKey().equals(k)) {
+        return pair;
+      }
+    }
+    return null;
   }
 
   @Override
   public int size() {
-    // TODO Implement Me!
-    return 0;
+    return size;
   }
 
   @Override
   public Iterator<K> iterator() {
     // TODO Implement Me!
     return null;
+  }
+
+  private static class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    Pair(K k, V v) {
+      key = k;
+      value = v;
+    }
+
+    public K getKey() {
+      return key;
+    }
+
+    public V getValue() {
+      return value;
+    }
   }
 }
